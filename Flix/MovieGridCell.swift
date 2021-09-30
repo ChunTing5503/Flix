d9af2aacf8d2e633620d03238427e169fe890fd3
cting5503 <cl5503@nyu.edu>
2021-09-30 21:53:43 +0400

    week 2 assignment

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Liu Chun Ting on 2021/9/29.
+//  Copyright © 2021 Liu Chun Ting. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
